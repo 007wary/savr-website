67f11a37052ca5122f5c5768494d25469f662ccc
Mwnswrang Wary <007mwnswrangwary@gmail.com>
2026-05-23 13:44:55 +0530

    fix: remove colgroup to fix table column alignment

--- components/WhySavr.js
+++ components/WhySavr.js
@@ -103,12 +103,6 @@ export default function WhySavr() {
           </p>
           <div style={{ overflowX: 'auto' }}>
             <table style={{ width: '100%', borderCollapse: 'collapse', minWidth: '500px', tableLayout: 'fixed' }}>
-              <colgroup>
-                <col style={{ width: '40%' }} />
-                <col style={{ width: '20%' }} />
-                <col style={{ width: '20%' }} />
-                <col style={{ width: '20%' }} />
-              </colgroup>
               <thead>
                 <tr>
                   {['Feature', 'Savr', 'Wallet', 'Axio'].map((h, i) => (
